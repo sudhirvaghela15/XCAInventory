eba9d5c842841968d6e5ab7b0a23165c5ec04419
sudhir <sudhirvaghela15@hotmail.com>
2025-07-06 00:18:12 +0530

    inventory form view created

--- XCAInventory/Module/Inventory form/View/InventoryFormView.swift
+++ XCAInventory/Module/Inventory form/View/InventoryFormView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct InventoryFormView: View {
+	
+	@StateObject var viewModel: InventoryFormViewModel = .init()
+	
+	@Environment(\.dismiss) var dismiss
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Form {
+			List {
+				inputSection
+			}
+		}.toolbar {
+			ToolbarItem(placement: .cancellationAction) {
+				Button("Cancel") {
+					dismiss()
+				}.disabled(viewModel.loadingState != .none)
+			}
+			
+			
+			ToolbarItem(placement: .confirmationAction) {
+				Button("Save") {
+					do {
+						try viewModel.save()
+						dismiss()
+					} catch {
+						
+					}
+				}.disabled(
+					viewModel.loadingState != .none || viewModel.name
+						.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+				)
+			}
+		}.alert(
+			isPresented: .constant(viewModel.error != nil),
+			error: "An Error Occured",
+			actions: { _ in },
+			message: { _ in
+				Text(viewModel.error.debugDescription)
+			}
+		)
+		.navigationTitle(viewModel.navigationTitle)
+		.navigationBarTitleDisplayMode(.inline)
     }
+	
+	var inputSection: some View {
+		Section {
+			TextField("Name", text: $viewModel.name)
+			Stepper("Quantity \(viewModel.quantity)", value: $viewModel.quantity)
+		}.disabled(viewModel.loadingState != .none)
+	}
 }
 
 #Preview {
--- XCAInventory/Module/Inventory form/ViewModel/InventoryFormViewModel.swift
+++ XCAInventory/Module/Inventory form/ViewModel/InventoryFormViewModel.swift
@@ -6,3 +6,109 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseStorage
+import SwiftUI
+import QuickLookThumbnailing
+
+final class InventoryFormViewModel: ObservableObject {
+	let db = Firestore.firestore()
+	let formType: FormType
+	let id: String
+	
+	@Published
+	var name: String = ""
+	
+	@Published
+	var quantity: Int = 0
+	
+	@Published
+	var usdzURL: URL?
+	
+	@Published
+	var thumbnailURL: URL?
+	
+	@Published
+	var loadingState: LoadingType = .none
+	
+	@Published
+	var error: Error?
+	
+	var navigationTitle: String {
+		switch formType {
+			case .add:
+				return "Add Item"
+			case .edit:
+				return "Edit Item"
+		}
+	}
+	
+	init(formType: FormType = .add) {
+		self.formType = formType
+		switch formType {
+			case .add:
+				id = UUID().uuidString
+			case .edit(let item):
+			id = item.id
+			name = item.name
+			quantity = item.quantity
+			
+			if let url = item.usdzURL {
+				self.usdzURL = url
+			}
+			
+			if let url = item.thumbnailURL {
+				self.thumbnailURL = url
+			}
+		}
+	}
+	
+	func save() throws {
+		loadingState = .savingItem
+		
+		defer { loadingState = .none }
+		
+		var item: InventoryItem
+		
+		switch formType {
+		case .add:
+			item = .init(id: id, name: name, quantity: quantity)
+			
+		case .edit(let inventoryItem):
+			item = inventoryItem
+			item.name = name
+			item.quantity = quantity
+		}
+		
+		item.usdzLink = usdzURL?.absoluteString
+		item.thumbnailLink = thumbnailURL?.absoluteString
+		
+		do {
+			try db.document("items/\(item.id)")
+				.setData(from: item, merge: true)
+		} catch {
+			self.error = error.localizedDescription
+			throw error
+		}
+	}
+}
+
+
+enum LoadingType: Equatable {
+	case none
+	case savingItem
+}
+
+enum FormType: Identifiable {
+	case add
+	case edit(InventoryItem)
+	
+	var id: String {
+		switch self {
+		case .add:
+			return "add"
+		case .edit(let inventoryItem):
+			return "edit\(inventoryItem.id)"
+		}
+	}
+}
