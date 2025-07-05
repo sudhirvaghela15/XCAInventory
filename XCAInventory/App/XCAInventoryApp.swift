b95ca3bd54466c831e80289c03cdb468b8dc44f8
sudhir <sudhirvaghela15@hotmail.com>
2025-07-05 22:31:10 +0530

    added view

---
+++ XCAInventory/App/XCAInventoryApp.swift
@@ -0,0 +1,20 @@
+//
+//  XCAInventoryApp.swift
+//  XCAInventory
+//
+//  Created by sudhir on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct XCAInventoryApp: App {
+	
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
