b4be2258351974278a6ddd1a483a95992e4134fe
sudhir <sudhirvaghela15@hotmail.com>
2025-07-05 15:04:05 +0530

    Initial Commit

---
+++ XCAInventory/XCAInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAInventoryApp.swift
+//  XCAInventory
+//
+//  Created by sudhir on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct XCAInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
