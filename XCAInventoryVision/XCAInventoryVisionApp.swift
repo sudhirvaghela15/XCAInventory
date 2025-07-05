8e579546581b0f7853ebffbdd99887289d756d86
sudhir <sudhirvaghela15@hotmail.com>
2025-07-05 15:41:21 +0530

    firebase dependency added

---
+++ XCAInventoryVision/XCAInventoryVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAInventoryVisionApp.swift
+//  XCAInventoryVision
+//
+//  Created by sudhir on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct XCAInventoryVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
