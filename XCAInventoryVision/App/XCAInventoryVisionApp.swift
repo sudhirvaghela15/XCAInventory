083ed89b0add895cd013f1ec049a12077ead9321
sudhir <sudhirvaghela15@hotmail.com>
2025-07-07 00:21:36 +0530

    create new windowgroup and injectiong navigation view model in environmental object

--- XCAInventoryVision/App/XCAInventoryVisionApp.swift
+++ XCAInventoryVision/App/XCAInventoryVisionApp.swift
@@ -11,12 +11,21 @@ import SwiftUI
 struct XCAInventoryVisionApp: App {
 	
 	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	@StateObject var navigationViewModel: NavigationViewModel = .init()
 	
     var body: some Scene {
         WindowGroup {
 			NavigationStack {
 				InventoryListView()
+					.environmentObject(navigationViewModel)
 			}
         }
+		
+		WindowGroup(id: "item") {
+			InventoryItemView()
+				.environmentObject(navigationViewModel)
+		}
+		.windowStyle(.volumetric)
+		.defaultSize(width: 1, height: 1, depth: 1, in: .meters)
     }
 }
