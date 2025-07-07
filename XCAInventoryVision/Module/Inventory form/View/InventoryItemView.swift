53194a45a23b6fa0e3f284449775b226859118b8
sudhir <sudhirvaghela15@hotmail.com>
2025-07-07 23:39:59 +0530

    added rotation and scale  animation in RealityView

--- XCAInventoryVision/Module/Inventory form/View/InventoryItemView.swift
+++ XCAInventoryVision/Module/Inventory form/View/InventoryItemView.swift
@@ -19,11 +19,26 @@ struct InventoryItemView: View {
 	@Environment(\.dismiss)
 	var dismiss
 	
+	// 3D ROTATION
+	@State
+	var angle: Angle = .degrees(0)
+	@State
+	var startAngle: Angle?
+	@State
+	var axis: (CGFloat, CGFloat, CGFloat) = (.zero, .zero, .zero)
+	@State
+	var startAxis: (CGFloat, CGFloat, CGFloat)?
+	
+	/// Scale Effect
+	@State
+	var scale: Double = 2
+	@State
+	var startScale: Double?
+	
     var body: some View {
 		ZStack(alignment: .bottom) {
-			RealityView { rvContent in
-			 
-			} update: { rvContent in
+			RealityView { _ in }
+			update: { rvContent in
 				if viewModel.entity == nil && !rvContent.entities.isEmpty {
 					rvContent.entities.removeAll()
 				}
@@ -37,6 +52,38 @@ struct InventoryItemView: View {
 					rvContent.add(entity)
 				}
 			}
+			.rotation3DEffect(angle, axis: axis)
+			.scaleEffect(scale)
+			.simultaneousGesture(DragGesture()
+				.onChanged({ value in
+					if let startAngle, let startAxis {
+						let _angle = sqrt(
+							pow(value.translation.width, 2) +
+							pow(value.translation.height, 2) +
+							startAngle.degrees
+						)
+						let axisX = ((-value.translation.height + startAxis.0) / CGFloat(_angle))
+						let axisY = ((value.translation.width + startAxis.1) / CGFloat(_angle))
+						angle = Angle(degrees: Double(_angle))
+						axis = (axisX, axisY, 0)
+					} else {
+						startAngle = angle
+						startAxis = axis
+					}}).onEnded({ value in
+						startAngle = angle
+						startAxis = axis
+					}))
+			.simultaneousGesture(MagnifyGesture()
+				.onChanged({ value in
+					if let startScale {
+						scale = max(1, min(3, value.magnification * startScale))
+					} else {
+						startScale = scale
+					}
+				}).onEnded({ value in
+					
+				})
+			).zIndex(1)
 			
 			VStack {
 				Text(viewModel.model?.name ?? "")
